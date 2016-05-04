06dca2ed4882dad6268f93390e70fd7e18e6802b
bright niyonzima <brightniyonzima@gmail.com>
2016-05-04 07:49:26 +0300

    solution 1

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+<title>highcharts</title>
+</head>
+<body>
+	<h3>HIGHCHART SOLUTIONS</h3>
+	<hr>
+	<ol>
+		<li><a href="line-basic.html">Solution 1 (basic line highcharts)</a></li>
+	</ol>
+</body>
+</html>
+<?php
+?>

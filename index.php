81ff66a95cce7291a4ce4a81caefbe6447d2516b
bright niyonzima <brightniyonzima@gmail.com>
2016-05-05 19:18:33 +0300

    updated index

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 		<li><a href="bar-basic.html">Solution 2 (basic bar highcharts)</a></li>
 		<li><a href="column-basic.html">Solution 3 (basic column highcharts)</a></li>
 		<li><a href="pie-basic.html">Solution 4 (basic pie highcharts)</a></li>
+		<li><a href="combined-basic.html">Solution 5 (combined highcharts)</a></li>
 	</ol>
 </body>
 </html>

8d31573a04e24c7fa93b0d12200d1ac5ad8aab0e
bright niyonzima <brightniyonzima@gmail.com>
2016-05-05 15:01:09 +0300

    updated index

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 	<hr>
 	<ol>
 		<li><a href="line-basic.html">Solution 1 (basic line highcharts)</a></li>
+		<li><a href="bar-basic.html">Solution 2 (basic bar highcharts)</a></li>
+		<li><a href="column-basic.html">Solution 3 (basic column highcharts)</a></li>
+		<li><a href="pie-basic.html">Solution 4 (basic pie highcharts)</a></li>
 	</ol>
 </body>
 </html>
